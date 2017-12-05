f8272a61eb16bd3480bcd20b1d22b832516e1727
shellscriptx <shellscriptx@gmail.com>
2017-12-05 18:15:25 -0200

    função getfqdn

--- src/os.sh
+++ src/os.sh
@@ -13,4 +13,22 @@ readonly __OS_SH=1
 
 source builtin.sh
 
-os.hostname(){ echo "$HOSTNAME"; return 0; }
+os.gethostname(){ echo "$HOSTNAME"; return 0; }
+
+os.getfqdn()
+{
+	local ip hosts host name
+
+	if [[ -e /etc/hosts ]]; then
+		while read ip host; do
+			if [[ "$ip" == "127.0.1.1" ]]; then
+				hosts=($host)
+				for name in ${hosts[@]}; do
+					echo "$name"
+				done				
+			fi
+		done < /etc/hosts
+	fi
+
+	return $?
+}
