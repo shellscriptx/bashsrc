10c5c30f8c29fa4f7d2afc4063b04db304a4b3d2
shellscriptx <shellscriptx@gmail.com>
2017-12-05 17:35:13 -0200

    função hostname

--- src/os.sh
+++ src/os.sh
@@ -13,31 +13,4 @@ readonly __OS_SH=1
 
 source builtin.sh
 
-
-readonly __HOSTNAME_FILE=/etc/hostname
-
-# access
-readonly F_OK=0
-readonly X_OK=1
-readonly W_OK=2
-readonly R_OK=4
-
-function os.hostname()
-{
-	getopt.parse "-:null:-:$*"
-	[[ -e $__HOSTNAME_FILE ]] && echo $(< $__HOSTNAME_FILE)
-	return 0
-}
-
-function os.access()
-{
-	getopt.parse "filename:str:+:$1" "flags:int:+:$2"
-	
-	local file=$1
-
-	if [[ -e $file ]]; then
-		:
-	else
-		:
-	fi
-}
+os.hostname(){ echo "$HOSTNAME"; return 0; }
