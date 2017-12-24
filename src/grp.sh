50464d3585a52ea851b128830a9054fa54bacedb
shellscriptx <shellscriptx@gmail.com>
2017-12-23 23:38:52 -0200

    biblioteca para obter informações de grupos do sistema

--- src/grp.sh
+++ src/grp.sh
@@ -1,130 +1,109 @@
 #!/bin/bash
 
+#----------------------------------------------#
+# Source:           grp.sh
+# Data:             23 de dezembro de 2017
+# Desenvolvido por: Juliano Santos [SHAMAN]
+# E-mail:           shellscriptx@gmail.com
+#----------------------------------------------#
+
 [[ $__GRP_SH ]] && return 0
 
 readonly __GRP_SH=1
 
 source builtin.sh
 
+readonly __GRP_PATH='/etc/group'
+readonly __GRP_ERR_READ_GROUP_FILE='falha ao ler o arquivo base'
+readonly __GRP_ERR_GROUP_NOT_FOUND='grupo não encontrado'
+
 # type group
 #
 # Implementa 'S' com os métodos:
 #
-# S.passwd => [str]
-# S.members => [str]
-# S.gid => [uint]
-# S.info => [str]
+# S.getgrgid
+# S.getgrusers
+# S.getgrpass
 #
+
+# func grp.getgrgid <[str]grpname> => [uint]
+#
+# Retorna o id associado a 'grpname'.
+#
+grp.getgrgid()
+{
+	getopt.parse "grpname:str:+:$1"
+	grp.___get_info "$1" gid
+	return $?
+}
+
+# func grp.getgrusers <[str]grpname> => [str]
+#
+# Retorna uma lista iterável com os usuários de 'grpname'.
+#
+grp.getgrusers()
+{
+	getopt.parse "grpname:str:+:$1"
+	grp.___get_info "$1" users
+	return $?
+}
+
+# func grp.getgrpass <[str]grpname> => [str]
+#
+# Retorna a flag de sinalização de senha
+#
+grp.getgrpass()
+{
+	getopt.parse "grpname:str:+:$1"
+	grp.___get_info "$1" pass
+	return $?
+}
 
 # func grp.getgrall => [str]
 #
-# Retorna uma lista iterável com o nome de todos os grupos do sistema.
+# Retorna uma lista iterável com todos os grupos do sistema.
 #
-function grp.getgrall()
+grp.getgrall()
 {
-    getopt.parse "-:null:-:$*"
+	getopt.parse "-:null:-:$*"
+	grp.___get_info '' all
+	return $?
+}
 
-    local grp IFSbkp
-	local filedb=/etc/group
+grp.___get_info()
+{
+	local group info fields
+	declare -A entry
 	
-    if [ -r "$filedb" ]; then
-        IFSbkp=$IFS
-        IFS=':'; while read grp _ _ _; do
-            echo "$grp"
-        done < $filedb
-        IFS=$IFSbkp
-    else
-        error.__exit '' '' '' "'$filedb' não foi possível ler o arquivo base"
-		err=1
-    fi
+	if while read group; do
+		entry[${group%%:*}]=${group//:/ }
+	done < $__GRP_PATH 2>/dev/null; then
+		
+		if [[ $2 == all ]]; then 
+			printf '%s\n' ${!entry[@]}
+			return 0
+		fi
 
-    return ${err:-0}
-}
+		if [[ ${entry[$1]} ]]; then
 
-# func grp.name <[uint]gid> => [str]
-#
-# Retorna o nome do grupo associado ao 'gid'.
-#
-function grp.name()
-{
-	getopt.parse "gid:uint:+:$1"
-	grp.__get_grp_info grp "$1"
-	return $?
-}
+			fields=(${entry[$1]})
 
-# func grp.passwd <[str]grpname> => [str]
-#
-# Retorna a flag de senha.
-#
-function grp.passwd()
-{
-	getopt.parse "grpname:str:+:$1"
-	grp.__get_grp_info pass "$1"
-	return $?
-}
+			case $2 in
+				pass) info=${fields[1]};;
+				gid) info=${fields[2]};;
+				users) info=${fields[@]:3}; info=${info//,/ };;
+				*) return 1;;
+			esac
 
-# func grp.members <[str]grpname> => [str]
-#
-# Retorna uma lista iterável com os membros de 'grpname'.
-#
-function grp.members()
-{
-	getopt.parse "grpname:str:+:$1"
-	printf '%s\n' $(grp.__get_grp_info mem "$1")
-	return $?
-}
+			printf '%s\n' $info
 
-# func grp.gid <[str]grpname> => [uint]
-#
-# Retorna o 'gid' de 'grpname' associado.
-#
-function grp.gid()
-{
-	getopt.parse "grpname:str:+:$1"
-	grp.__get_grp_info gid "$1"
-	return $?
-}
-
-# func grp.info <[str]grpname> => [str]
-#
-# Retorna todas as informações de 'grpname'.
-#
-function grp.info()
-{
-	getopt.parse "grpname:str:+:$1"
-	grp.__get_grp_info all "$1"
-	return $?
-}
-
-function grp.__get_grp_info()
-{
-	local grp pass gid mem err
-	local filedb=/etc/group
-	local IFSbkp=$IFS
-	local flag=$1
-	local field=$2
-
-	if [ -r "$filedb" ]; then	
-		IFS=':'; while read grp pass gid mem; do
-			if [[ "$field" == "$grp" || "$field" == "$gid" ]]; then
-				err=0
-				case $flag in
-					grp)	info=$grp; break;;
-					pass)	info=$pass; break;;
-					gid)	info=$gid; break;;
-					mem)	info=${mem//,/ }; break;;
-					all)	info="$grp|$pass|$gid|$mem"; break;;
-					*) return 1;;
-				esac
-			fi
-		done < $filedb
+		else
+			error.__exit 'group' 'str' "$1" "$__GRP_ERR_GROUP_NOT_FOUND"
+		fi	
 	else
-        error.__exit '' '' '' "'$filedb' não foi possível ler o arquivo base"
+		error.__exit '' '' "$__GRP_PATH" "$__GRP_ERR_READ_GROUP_FILE"
 	fi
 
-	IFS=$IFSbkp
-	
-	echo "$info"
-
-	return ${err:-1}
+	return $?
 }
+# /* __GRP_SH */
