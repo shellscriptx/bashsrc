75779f9dd52c35e8d393e817e7f0055b53040627
SHAMAN <juliano.santos.bm@gmail.com>
2018-02-09 15:47:47 -0200

    Adicionado as funções "error.code", "error.stack", "error.arg", "error.type", "error.value", "error.msg", "error.func" e "error.line"

--- src/error.sh
+++ src/error.sh
@@ -15,6 +15,7 @@ source struct.sh
 readonly __ERROR_SH=1
 
 var error_t struct_t
+var trace_t struct_t
 
 error_t.__add__ \
 	code 		uint \
@@ -31,43 +32,31 @@ error_t.__add__ \
 # false - Se ocorrer um erro, uma mensagem é exibida contendo as informações da
 # pilha de rastreamento e a execução do script é interrompida (padrão).
 #
-# true - Se ocorrer um erro, serão inicializadas as variáveis de rastreamento 
-# '__ERR_*' e o script continuará seu fluxo de execução.
+# true - Nâo interrompe a execução em caso de erro.
 #
-# Variáveis de rastreamento:
-#
-# __ERR__ - Status do erro
-# __ERR_STACK__ - Funções de desencadeamento
-# __ERR_ARG__ - Argumento da função
-# __ERR_TYPE__ - Tipo de dado do argumento
-# __ERR_VAL__ - Valor do argumento
-# __ERR_MSG__ - Mensagem de erro
-# __ERR_FUNC__ - Função que provocou o erro
-# __ERR_LINE__ - Número da linha onde o erro foi disparado.
-# 
 # Exemplo:
 #
 # #!/bin/bash
 #
 # source os.sh
 # 
-# var arq os.file
+# var arq file_t
 #
 # # Desabilitando o tratamento de erro
-# error.resume on
+# error.resume true
 #
 # # Tentando ler um arquivo que não existe.
 # os.open arq '/home/usuario/arquivo_nao_existe.txt' $O_RDONLY
 #
 # # Testa o status do erro 
-# if [ $__ERR__ ]; then
+# if [ $(error.code) -ne 0 ]]; then
 #     echo 'Ops !! Aconteceu algo !!'
-#     echo "Encontrei o erro -> $__ERR_MSG__"
-#     echo "Aconteceu aqui -> $__ERR_FUNC__"
+#     echo "Encontrei o erro -> $(error.msg)"
+#     echo "Aconteceu aqui -> $(error.func)"
 # fi
 #
 # Restaurando o tratamento de erro
-# error.resume off
+# error.resume false
 #
 # ----------------------
 # Saida:
@@ -104,17 +93,23 @@ function error.format()
 	return $?
 }
 
-function error.info()
+function error.errort()
 {
 	getopt.parse 1 "struct:error_t:+:$1" "${@:2}"
 	error.__output error_t "$1"
 	return $?
 }
 
+function error.errortf()
+{
+	getopt.parse -1 "struct:error_t:+:$1" "exp:str:-:$2" ... "${@:3}"
+	error.__output error_t "$1" "${@:2}"
+	return $?
+}
+
 function error.__output()
 {
-	local c_def c_red err_msg
-	local code line func err
+	local c_def c_red code line func err
 
 	if [[ $1 == error_t ]]; then
 		if [[ $(__type__ $2) == $1 ]]; then
@@ -134,7 +129,7 @@ function error.__output()
 	fi
 
 	case $__ON_ERROR_RESUME in
-		true)
+		true)	
 			__ERR__=${code:-$2}
 			__ERR_MSG__=${err:-$3}
 			__ERR_FUNC__=${func:-${FUNCNAME[2]}}
@@ -143,14 +138,14 @@ function error.__output()
 		false)
 			exec 1>&2
 			case $1 in
-				strerr)	err_msg=$3;;
-				warn)	c_red='\033[0;31m'; c_def='\033[0;m'; err_msg=$3;;
-				fmt)	err_msg=$3; printf -v err_msg "$err_msg" "${@:4}";;
-				error_t) ;;
-				*)		error.trace def; return $?;;
+				strerr)		err=$3;;
+				warn)		c_red='\033[0;31m'; c_def='\033[0;m'; err=$3;;
+				fmt)		err=$3; printf -v err "$err" "${@:4}";;
+				error_t) 	printf -v err "$err" "${@:3}";;
+				*)			error.trace def; return $?;;
 			esac
 				
-			echo -e "${c_red}${0##*/}: erro: linha ${line:-${BASH_LINENO[1]}}: ${func:-${FUNCNAME[2]}}: ${code:-$2}: ${err:-$err_msg} ${c_def}"
+			echo -e "${c_red}${0##*/}: erro: linha ${line:-${BASH_LINENO[1]}}: ${func:-${FUNCNAME[2]}}: ${code:-$2}: $err ${c_def}"
 			exit ${code:-$2}
 			;;
 	esac
@@ -176,7 +171,7 @@ function error.trace()
 
 	case $__ON_ERROR_RESUME in
 		true)	
-			__ERR__=${6:-1}
+			__ERR__=1
 			__ERR_STACK__=${stack% }
 			__ERR_ARG__=$2
 			__ERR_TYPE__=$3
@@ -234,6 +229,15 @@ function error.trace()
 
 	return 1
 }
+
+error.code(){ getopt.parse 0 "$@"; echo "${__ERR__:-0}"; return 0; }
+error.stack(){ getopt.parse 0 "$@"; echo "$__ERR_STACK__"; return 0; }
+error.arg(){ getopt.parse 0 "$@"; echo "$__ERR_ARG__"; return 0; }
+error.type(){ getopt.parse 0 "$@"; echo "$__ERR_TYPE__"; return 0; }
+error.value(){ getopt.parse 0 "$@"; echo "$__ERR_VAL__"; return 0; }
+error.msg(){ getopt.parse 0 "$@"; echo "$__ERR_MSG__"; return 0; }
+error.func(){ getopt.parse 0 "$@"; echo "$__ERR_FUNC__"; return 0; }
+error.line(){ getopt.parse 0 "$@"; echo "$__ERR_LINE__"; return 0; }
 
 source.__INIT__
 # /* __ERROR_SH */
