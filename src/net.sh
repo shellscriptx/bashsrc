eed15328815ade9b1233df5db67c4b9a8eac05b1
SHAMAN <juliano.santos.bm@gmail.com>
2018-02-23 18:17:48 -0300

    update

--- src/net.sh
+++ src/net.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#    Copyright 2018 Juliano Santos [SHAMAN]
+#
+#    This file is part of bashsrc.
+#
+#    bashsrc is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    bashsrc is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with bashsrc.  If not, see <http://www.gnu.org/licenses/>.
+
 [[ $__NET_SH ]] && return 0
 
 readonly __NET_SH=1
